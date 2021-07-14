8ba1397545442606ac1154cc7954c87e119dff91
z5260003 <z5260003@cse.unsw.edu.au>
2021-07-14 23:31:09 +1000

    CampFire draft 1

---
+++ src/unsw/loopmania/CampFire.java
@@ -0,0 +1,48 @@
+package unsw.loopmania;
+import java.util.List;
+
+import unsw.loopmania.LoopManiaWorld;
+import unsw.loopmania.StaticEntity;
+
+import java.util.ArrayList;
+
+public class CampFire extends Building{
+    private Character character;
+    private LoopManiaWorld world;
+    private ArrayList<StaticEntity> entities;
+
+    public CampFire(SimpleIntegerProperty x, SimpleIntegerProperty y, int radius){
+        super(x, y);
+        this.radius = radius;
+        this.entities = setEntities(x, y);
+    }
+
+    public int getRadius(){
+        return this.getRadius();
+    }
+
+    public void setEntities(SimpleIntegerProperty x, SimpleIntegerProperty y){
+        ArrayList<StaticEntity> a = new ArrayList<>();
+        int r = this.getRadius();
+        for(int x = this.getX() - r;x <= this.getX() + r; x++){
+            for(int y = this.getY() - r;y <= this.getY() + r; y++){
+                if(x*x + y*y <= r*r){
+                    StaticEntity b = new StaticEntity(x,y);
+                    a.add(b);
+                }
+            }
+        }
+        this.entities = b;
+    }
+
+    public ArrayList<StaticEntity> getEntities(){
+        return this.entities;
+    }
+
+    public void buffCharacter(LoopManiaWorld world){
+        StaticEntity c = new StaticEntity(world.getCharacter().getX(), world.getCharacter().getY());
+        if(this.getEntities.contains(c){
+            world.getCharacter().setDamage(world.getCharacter().getDamage()*2);
+        }
+    }
+}
