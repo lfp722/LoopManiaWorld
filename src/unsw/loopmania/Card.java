2b52bd2ea37f1115f5cdb119d222b54a9a5bd8e9
z5260003 <z5260003@cse.unsw.edu.au>
2021-07-15 13:06:23 +1000

    Card draft 1

--- src/unsw/loopmania/Card.java
+++ src/unsw/loopmania/Card.java
@@ -8,7 +8,22 @@ import javafx.beans.property.SimpleIntegerProperty;
  */
 public abstract class Card extends StaticEntity {
     // TODO = implement other varieties of card than VampireCastleCard
+    private Building building;
+    private LoopManiaWorld world;
+
     public Card(SimpleIntegerProperty x, SimpleIntegerProperty y) {
         super(x, y);
+    }
+
+    public void setBuildingType(Building building){
+        this.building = building;
+    }
+
+    public Building getBuildingType(){
+        return this.building;
+    }
+
+    public void createBuilding(SimpleIntegerProperty x, SimpleIntegerProperty y, LoopManiaWorld world){
+        world.convertCardToBuildingByCoordinates(this.getX(), this.getY(), this.getX(), this.getY())
     }
 }
