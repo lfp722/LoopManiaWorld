5f2bd441231009593005021d8e405f393ac77adc
Zhourui <z5227262@ad.unsw.edu.au>
2021-07-15 15:16:51 +0800

    properties for all

--- src/unsw/loopmania/items/rare/TheOneRing.java
+++ src/unsw/loopmania/items/rare/TheOneRing.java
@@ -1,24 +1,24 @@
 package unsw.loopmania.items.rare;
-
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import unsw.loopmania.items.Item;
 
 public class TheOneRing extends Item {
-    private double chanceAppearance;
+    private SimpleDoubleProperty chanceAppearance;
     public TheOneRing(SimpleIntegerProperty x, SimpleIntegerProperty y) {
         super(x, y);
-        this.chanceAppearance = 0.01;
+        this.chanceAppearance.set(0.01);
     }
     
     public void setChanceAppearance(double chanceAppearance) {
-        this.chanceAppearance = chanceAppearance;
+        this.chanceAppearance.set(chanceAppearance);
     }
     public double getChanceAppearance() {
-        return this.chanceAppearance;
+        return this.chanceAppearance.get();
     }
     public void appear(){
         double chanceTrance = Math.random();
-        if (chanceTrance <= this.chanceAppearance) {
+        if (chanceTrance <= this.chanceAppearance.get()) {
             return;
         }
         return;
