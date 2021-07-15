95f49a3b197c81d4b4911fd7a5f872712f0342f6
z5260003 <z5260003@cse.unsw.edu.au>
2021-07-15 22:57:40 +1000

    Fixed EnemyProducer

--- src/unsw/loopmania/ZombiePit.java
+++ src/unsw/loopmania/ZombiePit.java
@@ -9,7 +9,7 @@ public class ZombiePit extends Building implements EnemyProducer{
 
     @Override
     public Enemy EnemyProducer(LoopManiaWorld world) {
-        PathPosition pt = new PathPosition(0, Arrays.asList(new Pair<>(0, 1), new Pair<>(0, 2)));
+        PathPosition pt = new PathPosition(this.getPathTileNearBuilding(world), world.getOrderedPath());
         Zombie zombie = new Zombie(pt, world.getCycle());
         return zombie;
     }
