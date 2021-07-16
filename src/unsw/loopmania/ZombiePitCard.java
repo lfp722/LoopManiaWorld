ae783131967e497e90c51618720ffb787ac9fb62
Taehwan Kim <t.a.kim@student.unsw.edu.au>
2021-07-16 16:12:18 +1000

    ZombiePitCard added

---
+++ src/unsw/loopmania/ZombiePitCard.java
@@ -0,0 +1,11 @@
+package unsw.loopmania;
+
+import javafx.beans.property.SimpleIntegerProperty;
+
+public class ZombiePitCard extends Card{
+
+    public ZombiePitCard(SimpleIntegerProperty x, SimpleIntegerProperty y){
+        super(x,y);
+    }
+    
+}
