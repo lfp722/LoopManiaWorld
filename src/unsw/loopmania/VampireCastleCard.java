262b06146c6336dd18b8438ed4b8f0139b88b87e
Taehwan Kim <t.a.kim@student.unsw.edu.au>
2021-07-16 16:10:12 +1000

    VampireCastleCard added

---
+++ src/unsw/loopmania/VampireCastleCard.java
@@ -0,0 +1,11 @@
+package unsw.loopmania;
+
+import javafx.beans.property.SimpleIntegerProperty;
+
+public class VampireCastleCard extends Card{
+
+    public VampireCastleCard(SimpleIntegerProperty x, SimpleIntegerProperty y){
+        super(x,y);
+    }
+    
+}
