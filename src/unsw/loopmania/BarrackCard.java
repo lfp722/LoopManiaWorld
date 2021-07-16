10852e16032735d12e103cea0e98dc680cbba8fa
Taehwan Kim <t.a.kim@student.unsw.edu.au>
2021-07-16 16:08:19 +1000

    BarrackCard added

---
+++ src/unsw/loopmania/BarrackCard.java
@@ -0,0 +1,11 @@
+package unsw.loopmania;
+
+import javafx.beans.property.SimpleIntegerProperty;
+
+public class BarrackCard extends Card{
+
+    public BarrackCard(SimpleIntegerProperty x, SimpleIntegerProperty y){
+        super(x,y);
+    }
+    
+}
