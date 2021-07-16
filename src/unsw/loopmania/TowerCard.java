e47a172e90e6ffc345b5300d36d7f301b2a6d241
Taehwan Kim <t.a.kim@student.unsw.edu.au>
2021-07-16 16:09:02 +1000

    TowerCard added

---
+++ src/unsw/loopmania/TowerCard.java
@@ -0,0 +1,11 @@
+package unsw.loopmania;
+
+import javafx.beans.property.SimpleIntegerProperty;
+
+public class TowerCard extends Card{
+
+    public TowerCard (SimpleIntegerProperty x, SimpleIntegerProperty y){
+        super(x,y);
+    }
+    
+}
