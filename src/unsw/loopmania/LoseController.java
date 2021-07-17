edea0933a7e60db616bdd94f84fc16da63a15560
z5260003 <z5260003@cse.unsw.edu.au>
2021-07-17 19:30:07 +1000

    Lose Controller draft

---
+++ src/unsw/loopmania/LoseController.java
@@ -0,0 +1,16 @@
+package unsw.loopmania;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class LoseController {
+    private MenuSwitcher mainMenuSwitcher;
+
+    @FXML
+    private Button returnToMainMenu;
+
+    @FXML
+    private void switchToMainMenu(){
+        mainMenuSwitcher.switchMenu();
+    }
+}
