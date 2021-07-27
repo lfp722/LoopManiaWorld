b07505f8132ee5e9ebb09c07d51299c25b9e1056
z5260003 <z5260003@cse.unsw.edu.au>
2021-07-27 19:31:43 +1000

    LoseController.java sound updated

--- src/unsw/loopmania/LoseController.java
+++ src/unsw/loopmania/LoseController.java
@@ -5,16 +5,19 @@ import javafx.scene.control.Button;
 
 public class LoseController {
     private MenuSwitcher mainMenuSwitcher;
+    private Sound sound;
 
     @FXML
     private Button returnToMainMenu;
 
     @FXML
     private void switchToMainMenu(){
+        sound.stopLost();
         mainMenuSwitcher.switchMenu();
     }
 
     public void setWinMenu(MenuSwitcher menu) {
+        sound.stopLost();
         mainMenuSwitcher = menu;
     }
 }
