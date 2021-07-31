040fa686c5af586403517a46bb662a5a29dd085e
taketato <taketato@github.com>
2021-07-31 23:06:09 +1000

    covered more code of test

--- src/test/EquippedTest.java
+++ src/test/EquippedTest.java
@@ -8,6 +8,7 @@ import java.io.FileNotFoundException;
 import java.util.Iterator;
 
 import org.junit.jupiter.api.Test;
+import org.junit.platform.console.shadow.picocli.CommandLine.Help;
 
 import javafx.beans.property.SimpleIntegerProperty;
 import unsw.loopmania.*;
@@ -57,19 +58,28 @@ public class EquippedTest {
         eq.equipWeapon(anduril);
         assertEquals(expectedAttack, eq.getAttack());
         assertEquals(expectedAttack, eq.getDamage().get());
+        assertEquals(anduril, eq.getWeapon());
 
         expectedDefence = 8;
         eq.equipShield(shield);
         assertEquals(expectedDefence, eq.getDefence().get());
+        assertEquals(shield, eq.getShield());
         expectedDefence = 16;
         eq.equipHelmet(helmet);
         assertEquals(expectedDefence, eq.getDefence().get());
         assertEquals(originalAttack * 0.8, world.getCharacter().getAttr().getAttack().get());
+        assertEquals(helmet, eq.getHelmet());
         expectedDefence = 24;
         eq.equipArmour(armour);
         assertEquals(expectedDefence, eq.getDefence().get());
+        assertEquals(armour, eq.getArmour());
         expectedDefence = 36;
         eq.equipShield(tree);
         assertEquals(expectedDefence, eq.getDefence().get());
+        assertEquals(tree, eq.getShield());
+
+        String expected = new String("{\"weapon\":{\"level\":1,\"x\":0,\"y\":1,\"type\":\"Anduril\",\"raretype\":-1},\"shield\":{\"level\":1,\"x\":0,\"y\":1,\"type\":\"TreeStump\",\"raretype\":-1},\"ring\":{},\"helmet\":{\"level\":1,\"x\":0,\"y\":1,\"type\":\"Helmet\"},\"armour\":{\"level\":1,\"x\":0,\"y\":1,\"type\":\"Armour\"}}");
+        assertEquals(expected, eq.toJSON().toString());
     }
+
 }
